2609252ca855f6566c3324a377dfd9d3c43b4870
grtumanyan <grtumanyan@gmail.com>
2018-12-06 13:41:28 +0400

    [FEAT]: ATP-108 make donation bottom section as featured content

---
+++ src/Migrations/Version20181206093511.php
@@ -0,0 +1,28 @@
+<?php declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20181206093511 extends AbstractMigration
+{
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE donation_bottom ADD date_created DATE NOT NULL, ADD link_type LONGTEXT NOT NULL, ADD link LONGTEXT NOT NULL, ADD position LONGTEXT NOT NULL');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE donation_bottom DROP date_created, DROP link_type, DROP link, DROP position');
+    }
+}
