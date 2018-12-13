655bebd318c41de6d6aab1256c383bc39ad23837
grtumanyan <grtumanyan@gmail.com>
2018-12-13 13:33:31 +0400

    [TEST]: Testing PayPal

--- src/Controller/PaypalController.php
+++ src/Controller/PaypalController.php
@@ -98,7 +98,7 @@ class PaypalController extends AbstractController
         $curl = curl_init();
 
         curl_setopt_array($curl, array(
-            CURLOPT_URL => "http://www.paypal.com/XMLPay",
+            CURLOPT_URL => "https://pilot-payflowpro.paypal.com",
             CURLOPT_RETURNTRANSFER => true,
             CURLOPT_ENCODING => "",
             CURLOPT_MAXREDIRS => 10,
