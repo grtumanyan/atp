4d6ff7e6aa1e95ac0e5a68466825c68916c7c189
grtumanyan <grtumanyan@gmail.com>
2018-12-13 12:06:23 +0400

    [TEST]: Testing paypal xmlpay request

--- src/Controller/PaypalController.php
+++ src/Controller/PaypalController.php
@@ -89,4 +89,38 @@ class PaypalController extends AbstractController
             'id' => $id
         ]);
     }
+
+    /**
+     * @Route("/test", name="test")
+     */
+    public function test(Request $request)
+    {
+        $curl = curl_init();
+
+        curl_setopt_array($curl, array(
+            CURLOPT_URL => "http://www.paypal.com/XMLPay",
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => "",
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 30,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => "POST",
+            CURLOPT_POSTFIELDS => "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\r\n<XMLPayRequest Timeout=\"30\" version = \"2.0\" xmlns=\"http://www.paypal.com/XMLPay\">\r\n  <RequestData>\r\n      <Vendor>armeniatree1994</Vendor>\r\n      <Partner>VeriSign</Partner>\r\n      <Transactions>\r\n           <Transaction>\r\n               <Sale>\r\n                    <PayData>\r\n                         <Invoice>\r\n                              <NationalTaxIncl>false</NationalTaxIncl>\r\n                              <TotalAmt>50.00</TotalAmt>\r\n                         </Invoice>\r\n                         <Tender>\r\n                              <Card>\r\n                                   <CardType>mastercard</CardType>\r\n                                   <CardNum>9999888877772222</CardNum>\r\n                                   <ExpDate>209912</ExpDate>\r\n                                   <NameOnCard>Edward Someone</NameOnCard>\r\n                              </Card>\r\n                         </Tender>\r\n                    </PayData>\r\n               </Sale>\r\n           </Transaction>\r\n      </Transactions>\r\n  </RequestData>\r\n   <RequestAuth>\r\n      <UserPass>\r\n           <User>armeniatree1994</User>\r\n           <Password>Begreen20</Password>\r\n      </UserPass>\r\n  </RequestAuth>\r\n</XMLPayRequest>",
+            CURLOPT_HTTPHEADER => array(
+                "cache-control: no-cache"
+            ),
+        ));
+
+        $response = curl_exec($curl);
+        $err = curl_error($curl);
+
+        curl_close($curl);
+
+        if ($err) {
+            echo "cURL Error #:" . $err;
+        } else {
+            echo $response;
+        }
+
+    }
 }
