af9318f743d34910b5e2691282ac448be97709ce
dkroyan <dkroyan@gmail.com>
2018-12-17 19:26:52 +0400

    Remove slick slider script to fruit harvesting page

--- public/js/sliderFunctionality.js
+++ public/js/sliderFunctionality.js
@@ -11,29 +11,3 @@ $(document).ready(function() {
     })
 })
 
-
-
-
-$('.before-after-slider').slick({
-    arrows: true,
-    dots: false,
-    infinite: true,
-    speed: 600,
-    autoplay: false,
-    draggable: false,
-    autoplaySpeed: 2000,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-});
-
-// $('.bb-interactive-slider').slick({
-//     arrows: false,
-//     dots: true,
-//     infinite: true,
-//     speed: 600,
-//     autoplay: true,
-//     autoplaySpeed: 5000,
-//     slidesToShow: 1,
-//     slidesToScroll: 1,
-//     adaptiveHeight: true,
-// });
